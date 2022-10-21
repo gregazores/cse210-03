58aab89aaa428c30f682e5cb58407864f7747c1b
Jared Martínez <jaredartt@gmail.com>
2022-10-21 10:48:06 +0200

    Input class

--- jumper/game_classes/input.py
+++ jumper/game_classes/input.py
@@ -10,3 +10,10 @@ class Input:
         -get: returns the player's input as a string 
         -set: sets the input attribute 
     """
+    def get_input():
+        set_input(player_input)
+        return player_input
+    
+    def set_input(self):
+        self.player_input = input('Guess a letter (a-z): ')
+        return self.player_input
